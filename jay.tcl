6a80b4e9e45abe76ef1894a46132243afd4e3f17
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-04-19 08:07:45 +0200

    load the dependencies

--- jay.tcl
+++ jay.tcl
@@ -7,4 +7,32 @@
 #   1 --> The exit was caused by an error generated for an expected   reason.
 #   2 --> The exit was caused by an error generated for an unexpected reason.
 
+# Load the dependencies.
+package require Tk
+package require Img
+package require msgcat
+
+switch -- $tcl_version {
+    "8.6" { package require tksvg }
+}
+
+# Check the executable path.
+set executable_path [info nameofexecutable]
+switch -glob -- [file tail $executable_path] {
+    ""  {
+        puts stdout "Cannot identify the application interpreter."
+        exit 1
+    }
+    "wish*" {
+        # The application was builded as a package.
+        set ::BUILDED_AS package
+    }
+    default {
+        # The application was builded as a standalone executable file.
+        package require starkit
+
+        set ::BUILDED_AS standalone
+    }
+}
+
 #*EOF*
