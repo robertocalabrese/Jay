3a492879045e8e3ae22f140f98a2bc5e7200f156
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-04-19 08:07:12 +0200

    add 'jay.tcl'

---
+++ jay.tcl
@@ -0,0 +1,10 @@
+# Developer:  🆁🅾🅱🅴🆁🆃🅾  🅲🅰🅻🅰🅱🆁🅴🆂🅴
+# City:       🅿🅰🅻🅴🆁🅼🅾, 🆂🅸🅲🅸🅻🆈
+# Country:    🅸🆃🅰🅻🆈
+
+# Exit codes:
+#   0 --> The exit was not caused by an error.
+#   1 --> The exit was caused by an error generated for an expected   reason.
+#   2 --> The exit was caused by an error generated for an unexpected reason.
+
+#*EOF*
