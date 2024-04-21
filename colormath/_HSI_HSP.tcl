ab6c1e085f3034ac2a7b23ae516fcbc1588aebac
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-04-21 10:20:07 +0200

    improving readability

--- colormath/_HSI_HSP.tcl
+++ colormath/_HSI_HSP.tcl
@@ -30,6 +30,10 @@
 #   the saturation channel in the range [0,100.0],
 #   the perceived brightness channel in the range [0,100.0].
 proc ::_HSI_HSP { channels } {
+    set Yr $::sRGB(unadapted,Yr)
+    set Yg $::sRGB(unadapted,Yg)
+    set Yb $::sRGB(unadapted,Yb)
+
     foreach { hue saturation intensity } $channels {
         set s [expr { $saturation*0.01 }]; # [0,1.0]
         set i [expr { $intensity*0.01 }]; # [0,1.0]
@@ -95,7 +99,7 @@ proc ::_HSI_HSP { channels } {
             set b 1.0
         }
 
-        set perceived_brightness [expr { (sqrt(($r*$r*$::sRGB(unadapted,Yr))+($g*$g*$::sRGB(unadapted,Yg))+($b*$b*$::sRGB(unadapted,Yb))))*100.0 }]
+        set perceived_brightness [expr { (sqrt(($r*$r*$Yr)+($g*$g*$Yg)+($b*$b*$Yb)))*100.0 }]
 
         # Adjust the perceived brightness value if exceeds its limits [0,100.0].
         if { $perceived_brightness < 0 } {
