c6f3bb338e135dded4615795cd644b5a40fecf7d
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-04-22 11:21:54 +0200

    add 'CALLER_INFO'

--- cmds/color.tcl
+++ cmds/color.tcl
@@ -4,7 +4,10 @@
 
 ## color - A command to deal with colors.
 proc color { action { args "" } } {
-
+    # If an error is prevented to happen (for example due to bad options/values provided),
+    # a fatal error window will be raised with all the information related to the caller
+    # of this command ('CALLER_INFO').
+    set CALLER_INFO [info frame -1]
 }
 
 #*EOF*
