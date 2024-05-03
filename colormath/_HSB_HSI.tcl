0952fd70958db2ab3730c816c625ff58d0e3b7b6
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:07:41 +0200

    refactor: HSB to HSI conversion (without alpha channel)

--- colormath/_HSB_HSI.tcl
+++ colormath/_HSB_HSI.tcl
@@ -4,33 +4,58 @@
 
 # ::_HSB_HSI
 #
-# Transform HSB channels into HSI channels.
+# Transform HSB colors (without alpha channel) into HSI colors (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the HSB channels to convert, with:
-#                   the hue channel in the range [0,360[,
-#                   the saturation channel in the range [0,100.0],
-#                   the brightness channel in the range [0,100.0].
+# channels      Should be a list that specifies all the channels (flattened together) of the HSB colors to convert.
+#               Each HSB color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   H --> Hue        [0,360.0[ --> Note: '360.0' is not included.
+#                   S --> Saturation [0,100.0]
+#                   B --> Brightness [0,100.0]
 #
-# Note:  Some pre-computation have been made in order to increase the performance:
-#           1.0/3.0   = 0.3333333333333333
-#           1.0/6.0   = 0.16666666666666666
-#           1.0/100.0 = 0.01
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
 #
-# Returns a list containing the resulting HSI channels, with:
-#   the hue channel in the range [0,360[,
-#   the saturation channel in the range [0,100.0],
-#   the intensity channel in the range [0,100.0].
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 120 50 50]
+#                       channels --> [list 120 50 50]
+#
+#                   Two colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       channels --> [list 120 50 50 57 80 80] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       color3   --> [list 270 20 90]
+#                       channels --> [list 120 50 50 57 80 80 270 20 90] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# Some pre-computation have been made in order to increase the performance:
+#   1 / 3   = 0.3333333333333333
+#   1 / 6   = 0.16666666666666666
+#   1 / 100 = 0.01
+#
+# Return a list containing the resulting HSI colors channels (flattened together).
+# Each HSI color will be rappresented by 3 channels values in the following order and ranges:
+#   H --> Hue        [0,360.0[ --> Note: '360.0' is not included.
+#   S --> Saturation [0,100.0]
+#   I --> Intensity  [0,100.0]
 proc ::_HSB_HSI { channels } {
     foreach { hue saturation brightness } $channels {
-        set s [expr { $saturation*0.01 }]; # [0,1.0]
-        set b [expr { $brightness*0.01 }]; # [0,1.0]
+        set s [expr { $saturation*0.01 }]; # range [0,1.0]
+        set b [expr { $brightness*0.01 }]; # range [0,1.0]
 
         set h [expr { int($hue*0.016666666666666666)%6 }]
         set f [expr { ($hue*0.016666666666666666)-$h }]
         set p [expr { $b*(1.0-$s) }]
 
+        # Compute the rgb values [0,1.0].
         switch -- $h {
             0   {
                 set k [expr { 1.0-((1.0-$f)*$s) }]
@@ -70,51 +95,18 @@ proc ::_HSB_HSI { channels } {
             }
         }
 
-        # Adjust the rgb channels values if they exceeds their limits [0,1.0].
-        if { $r < 0 } {
-            set r 0
-        } elseif { $r > 1.0 } {
-            set r 1.0
-        }
-
-        if { $g < 0 } {
-            set g 0
-        } elseif { $g > 1.0 } {
-            set g 1.0
-        }
-
-        if { $b < 0 } {
-            set b 0
-        } elseif { $b > 1.0 } {
-            set b 1.0
-        }
-
         # Compute the intensity [0,100.0].
-        set i         [expr { ($r+$g+$b)*0.3333333333333333 }]; # [0,1.0]
-        set intensity [expr { $i*100.0 }]; # [0,100.0]
+        set i         [expr { ($r+$g+$b)*0.3333333333333333 }]; # range [0,1.0]
+        set intensity [expr { $i*100.0 }]
 
-        # Adjust the intensity value if exceeds its limits [0,100.0].
-        if { $intensity < 0 } {
-            set intensity 0
-        } elseif { $intensity > 100.0 } {
-            set intensity 100.0
-        }
-
+        # Compute the saturation [0,100.0].
         if { $r == $g && $r == $b } {
             # It's a gray...
             set hue        0
             set saturation 0
         } else {
-            # Compute the saturation [0,100.0].
             set min        [expr { min($r,$g,$b) }]
             set saturation [expr { (1.0-($min/$i))*100.0 }]
-        }
-
-        # Adjust the saturation value if exceeds its limits [0,100.0].
-        if { $saturation < 0 } {
-            set saturation 0
-        } elseif { $saturation > 100.0 } {
-            set saturation 100.0
         }
 
         lappend results $hue $saturation $intensity
