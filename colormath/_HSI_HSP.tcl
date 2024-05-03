623ef1915075a4085e08449c7633294039b04aa6
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:08:30 +0200

    refactor: HSI to HSP conversion (without alpha channel)

--- colormath/_HSI_HSP.tcl
+++ colormath/_HSI_HSP.tcl
@@ -4,44 +4,71 @@
 
 # ::_HSI_HSP
 #
-# Transform HSI channels into HSP channels.
+# Transform HSI colors (without alpha channel) into HSP colors (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the HSI channels to convert, with:
-#                   the hue channel in the range [0,360[,
-#                   the saturation channel in the range [0,100.0],
-#                   the intensity channel in the range [0,100.0].
+# channels      Should be a list that specifies all the channels (flattened together) of the HSI colors to convert.
+#               Each HSI color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   H --> Hue        [0,360.0[ --> Note: '360.0' is not included.
+#                   S --> Saturation [0,100.0]
+#                   I --> Intensity  [0,100.0]
 #
-# Note:  Some pre-computation have been made in order to increase the performance:
-#           π/180.0   = 0.017453292519943295
-#           1.0/100.0 = 0.01
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
 #
-#           1.0471975511965976 = 60  degrees expressed in radians.
-#           2.0943951023931953 = 120 degrees expressed in radians.
-#           3.141592653589793  = 180 degrees expressed in radians.
-#           4.1887902047863905 = 240 degrees expressed in radians.
-#           5.235987755982989  = 300 degrees expressed in radians.
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 120 50 50]
+#                       channels --> [list 120 50 50]
+#
+#                   Two colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       channels --> [list 120 50 50 57 80 80] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       color3   --> [list 270 20 90]
+#                       channels --> [list 120 50 50 57 80 80 270 20 90] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# Some pre-computation have been made in order to increase the performance:
+#   1 / 100 = 0.01
+#   π / 180 = 0.017453292519943295
+#
+#   1.0471975511965976 = 60  degrees expressed in radians.
+#   2.0943951023931953 = 120 degrees expressed in radians.
+#   3.141592653589793  = 180 degrees expressed in radians.
+#   4.1887902047863905 = 240 degrees expressed in radians.
+#   5.235987755982989  = 300 degrees expressed in radians.
 #
 # Note:  For info about unadapted values visit 'http://www.brucelindbloom.com'.
 #
-# Returns a list containing the resulting HSP channels, with:
-#   the hue channel in the range [0,360[,
-#   the saturation channel in the range [0,100.0],
-#   the perceived brightness channel in the range [0,100.0].
+# Note:  For info about the HSP color system visit 'https://www.alienryderflex.com/hsp.html'.
+#
+# Return a list containing the resulting HSP colors channels (flattened together).
+# Each HSP color will be rappresented by 3 channels values in the following order and ranges:
+#   H --> Hue                  [0,360.0[ --> Note: '360.0' is not included.
+#   S --> Saturation           [0,100.0]
+#   P --> Perceived Brightness [0,100.0]
 proc ::_HSI_HSP { channels } {
     set Yr $::sRGB(unadapted,Yr)
     set Yg $::sRGB(unadapted,Yg)
     set Yb $::sRGB(unadapted,Yb)
 
     foreach { hue saturation intensity } $channels {
-        set s [expr { $saturation*0.01 }]; # [0,1.0]
-        set i [expr { $intensity*0.01 }]; # [0,1.0]
+        set s [expr { $saturation*0.01 }]; # range [0,1.0]
+        set i [expr { $intensity*0.01  }]; # range [0,1.0]
 
         set k0 [expr { $i*$s }]
         set k1 [expr { $i-$k0 }]
         set k2 [expr { 2.0*$k0 }]
 
+        # Compute the rgb values [0,1.0].
         if { $hue == 0 } {
             set r [expr { $i+$k2 }]
             set g $k1
@@ -80,36 +107,11 @@ proc ::_HSI_HSP { channels } {
             set b [expr { $i+($k2*$interim) }]
         }
 
-        # Adjust the rgb channels values if they exceeds their limits [0,1.0].
-        if { $r < 0 } {
-            set r 0
-        } elseif { $r > 1.0 } {
-            set r 1.0
-        }
-
-        if { $g < 0 } {
-            set g 0
-        } elseif { $g > 1.0 } {
-            set g 1.0
-        }
-
-        if { $b < 0 } {
-            set b 0
-        } elseif { $b > 1.0 } {
-            set b 1.0
-        }
-
+        # Compute the perceived brightness [0,100.0].
         set perceived_brightness [expr { (sqrt(($r*$r*$Yr)+($g*$g*$Yg)+($b*$b*$Yb)))*100.0 }]
 
-        # Adjust the perceived brightness value if exceeds its limits [0,100.0].
-        if { $perceived_brightness < 0 } {
-            set perceived_brightness 0
-        } elseif { $perceived_brightness > 100.0 } {
-            set perceived_brightness 100.0
-        }
-
+        # Compute the saturation [0,100.0].
         set max [expr { max($r,$g,$b) }]
-
         if { $r == $g && $r == $b } {
             # It's a grey...
             set hue        0
@@ -136,13 +138,6 @@ proc ::_HSI_HSP { channels } {
                 # g >= r
                 set saturation [expr { (1.0-($r/$b))*100.0 }]
             }
-        }
-
-        # Adjust the saturation value if exceeds its limits [0,100.0].
-        if { $saturation < 0 } {
-            set saturation 0
-        } elseif { $saturation > 100.0 } {
-            set saturation 100.0
         }
 
         lappend results $hue $saturation $perceived_brightness
