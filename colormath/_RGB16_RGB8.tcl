8c8e26144d0178d0f88ab1456dff8a674dab5a53
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:20:10 +0200

    refactor: RGB16 to RGB8 conversion (without alpha channel)

--- colormath/_RGB16_RGB8.tcl
+++ colormath/_RGB16_RGB8.tcl
@@ -4,23 +4,53 @@
 
 # ::_RGB16_RGB8
 #
-# Transform RGB channels at 16 bit into RGB channels at 8 bit.
+# Transform RGB colors at 16 bit (without alpha channel) into RGB colors at 8 bit (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the RGB channels values to convert [0,65535].
+# channels      Should be a list that specifies all the channels (flattened together) of the RGB colors at 16 bit to convert.
+#               Each RGB color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   R --> Red   [0,65535]
+#                   G --> Green [0,65535]
+#                   B --> Blue  [0,65535]
 #
-# Note:  A pre-computation has been made in order to increase the performance:
-#           255.0 / 65535.0  = 0.0038910505836575876
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
 #
-# Returns a list containing the resulting RGB channels, with each channel in the range [0,255].
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 3400 31720 32120]
+#                       channels --> [list 3400 31720 32120]
+#
+#                   Two colors:
+#                       color1   --> [list 3400 31720 32120]
+#                       color2   --> [list 3570 3650  3650 ]
+#                       channels --> [list 3400 31720 32120 3570 3650 3650] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 3400 31720 32120]
+#                       color2   --> [list 3570 3650  3650 ]
+#                       color3   --> [list 3230 32120 31200]
+#                       channels --> [list 3400 31720 32120 3570 3650 3650 3230 32120 31200] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# A pre-computation has been made in order to increase the performance:
+#   255 / 65535 = 0.0038910505836575876
+#
+# Return a list containing the resulting RGB colors channels (flattened together) at 8 bit.
+# Each RGB color will be rappresented by 3 channels values in the following order and ranges:
+#   R --> Red   [0,255]
+#   G --> Green [0,255]
+#   B --> Blue  [0,255]
 proc ::_RGB16_RGB8 { channels } {
-    foreach { red green blue } $channels {
-        set red   [expr { round($red*0.0038910505836575876) }]
-        set green [expr { round($green*0.0038910505836575876) }]
-        set blue  [expr { round($blue*0.0038910505836575876) }]
+    foreach { red16 green16 blue16 } $channels {
+        set red8   [expr { round($red16*0.0038910505836575876)   }]
+        set green8 [expr { round($green16*0.0038910505836575876) }]
+        set blue8  [expr { round($blue16*0.0038910505836575876)  }]
 
-        lappend results $red $green $blue
+        lappend results $red8 $green8 $blue8
     }
 
     return $results
