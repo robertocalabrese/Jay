9ff8ce217dd915a38309a765e44aeca660583517
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:14:12 +0200

    refactor: rgb to HSL conversion (without alpha channel)

--- colormath/_rgb_HSL.tcl
+++ colormath/_rgb_HSL.tcl
@@ -4,32 +4,52 @@
 
 # ::_rgb_HSL
 #
-# Transform rgb channels into HSL channels.
+# Transform rgb colors (without alpha channel) into HSL colors (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the rgb channels values to convert [0,1.0].
+# channels      Should be a list that specifies all the channels (flattened together) of the rgb colors to convert.
+#               Each rgb color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   r --> red   [0,1.0]
+#                   g --> green [0,1.0]
+#                   b --> blue  [0,1.0]
 #
-# Returns a list containing the resulting HSL channels, with:
-#   the hue channel in the range [0,360[,
-#   the saturation channel in the range [0,100.0],
-#   the lightness channel in the range [0,100.0].
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
+#
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 0.4 0.5 0.2]
+#                       channels --> [list 0.4 0.5 0.2]
+#
+#                   Two colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       color3   --> [list 0.2 0.2 0.1]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6 0.2 0.2 0.1] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# Return a list containing the resulting HSL colors channels (flattened together).
+# Each HSL color will be rappresented by 3 channels values in the following order and ranges:
+#   H --> Hue        [0,360.0[ --> Note: '360.0' is not included.
+#   S --> Saturation [0,100.0]
+#   L --> Lightness  [0,100.0]
 proc ::_rgb_HSL { channels } {
     foreach { r g b } $channels {
         set min  [expr { min($r,$g,$b) }]
         set max  [expr { max($r,$g,$b) }]
-        set sum  [expr { $max+$min }]
-        set diff [expr { $max-$min }]
+        set sum  [expr { $max+$min     }]
+        set diff [expr { $max-$min     }]
 
         # Compute the lightness [0,100.0].
         set lightness [expr { $sum*50.0 }]
-
-        # Adjust the lightness value if exceeds its limits [0,100.0].
-        if { $lightness < 0 } {
-            set lightness 0
-        } elseif { $lightness > 100.0 } {
-            set lightness 100.0
-        }
 
         switch -- $diff {
             0   {
@@ -45,14 +65,8 @@ proc ::_rgb_HSL { channels } {
                     set saturation [expr { ($diff/$sum)*100.0 }]
                 }
 
-                # Adjust the saturation value if exceeds its limits [0,100.0].
-                if { $saturation < 0 } {
-                    set saturation 0
-                } elseif { $saturation > 100.0 } {
-                    set saturation 100.0
-                }
-
                 # Compute the hue [0,360.0[.
+                # '360.0' is not included.
                 if { $r == $max } {
                     set segment [expr { ($g-$b)/$diff }]
 
@@ -69,13 +83,6 @@ proc ::_rgb_HSL { channels } {
                 } else {
                     set segment [expr { ($r-$g)/$diff }]
                     set hue     [expr { (4.0+$segment)*60.0 }]
-                }
-
-                # Adjust the hue value if exceeds its limits [0,360[.
-                if { $hue < 0 } {
-                    set hue [expr { $hue+360.0 }]
-                } elseif { $hue >= 360.0 } {
-                    set hue [expr { $hue-360.0 }]
                 }
             }
         }
