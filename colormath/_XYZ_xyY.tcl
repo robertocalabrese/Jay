76e466bb58a7878cc2ff191cb005a86a6a47826f
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:13:09 +0200

    refactor: XYZ to xyY conversion (without alpha channel)

--- colormath/_XYZ_xyY.tcl
+++ colormath/_XYZ_xyY.tcl
@@ -4,16 +4,44 @@
 
 # ::_XYZ_xyY
 #
-# Transform XYZ channels into xyY channels (both in the PCS D50 space).
+# Transform XYZ colors (without alpha channel) into xyY colors (without alpha channel).
+# Both in the PCS D50 space.
 #
 # Where:
 #
-# channels      Should be a list containing the XYZ channels values to convert, with:
-#                   the X channels in the range [0,$::sRGB(PCS,X)],
-#                   the Y channels in the range [0,$::sRGB(PCS,Y)],
-#                   the Z channels in the range [0,$::sRGB(PCS,Z)].
+# channels      Should be a list that specifies all the channels (flattened together) of the XYZ colors to convert.
+#               Each XYZ color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   X --> X [0,$sRGB(PCS,X)]
+#                   Y --> Y [0,$sRGB(PCS,Y)]
+#                   Z --> Z [0,$sRGB(PCS,Z)]
 #
-# Returns a list containing the resulting xyY channels, with each channel in the range [0,1.0].
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
+#
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 0.4 0.5 0.2]
+#                       channels --> [list 0.4 0.5 0.2]
+#
+#                   Two colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       color3   --> [list 0.2 0.2 0.1]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6 0.2 0.2 0.1] <-- all colors channels must be flattened together.
+#
+# Note:  For info about XYZ to xyY conversions visit 'http://www.brucelindbloom.com'.
+#
+# Return a list containing the resulting xyY colors channels (flattened together).
+# Each xyY color will be rappresented by 3 channels values in the following order and ranges:
+#   x --> x [0,1.0]
+#   y --> y [0,1.0]
+#   Y --> Y [0,1.0]
 proc ::_XYZ_xyY { channels } {
     foreach { X Y Z } $channels {
         if { $X == 0 && $Y == 0 && $Z == 0 } {
@@ -21,17 +49,11 @@ proc ::_XYZ_xyY { channels } {
             set x $::sRGB(PCS,x)
             set y $::sRGB(PCS,y)
         } else {
+            # Compute x [0,1.0].
             set x [expr { $X/($X+$Y+$Z) }]
+
+            # Compute y [0,1.0].
             set y [expr { $Y/($X+$Y+$Z) }]
-
-            # Adjust the x and y values if they exceeds their limits [0,1.0].
-            if { $x > 1.0 } {
-                set x 1.0
-            }
-
-            if { $y > 1.0 } {
-                set y 1.0
-            }
         }
 
         lappend results $x $y $Y
