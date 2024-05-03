96d5b2b8f8e9ad529a4f1298340a353c052ffd35
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:16:01 +0200

    refactor: RGB12 to rgb conversion (without alpha channel)

--- colormath/_RGB12_rgb.tcl
+++ colormath/_RGB12_rgb.tcl
@@ -4,40 +4,51 @@
 
 # ::_RGB12_rgb
 #
-# Transform RGB channels at 12 bit into rgb channels.
+# Transform RGB colors at 12 bit (without alpha channel) into rgb colors (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the RGB channels values to convert [0,4095].
+# channels      Should be a list that specifies all the channels (flattened together) of the RGB colors at 12 bit to convert.
+#               Each RGB color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   R --> Red   [0,4095]
+#                   G --> Green [0,4095]
+#                   B --> Blue  [0,4095]
 #
-# Note:  A pre-computation has been made in order to increase the performance:
-#           1.0 / 4095.0 = 0.0002442002442002442
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
 #
-# Returns a list containing the resulting rgb channels, with each channel in the range [0,1.0].
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 400 1720 2120]
+#                       channels --> [list 400 1720 2120]
+#
+#                   Two colors:
+#                       color1   --> [list 400 1720 2120]
+#                       color2   --> [list 570 650  650 ]
+#                       channels --> [list 400 1720 2120 570 650 650] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 400 1720 2120]
+#                       color2   --> [list 570 650  650 ]
+#                       color3   --> [list 230 2120 1200]
+#                       channels --> [list 400 1720 2120 570 650 650 230 2120 1200] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# A pre-computation has been made in order to increase the performance:
+#   1 / 4095 = 0.0002442002442002442
+#
+# Return a list containing the resulting rgb colors channels (flattened together).
+# Each rgb color will be rappresented by 3 channels values in the following order and ranges:
+#   r --> red   [0,1.0]
+#   g --> green [0,1.0]
+#   b --> blue  [0,1.0]
 proc ::_RGB12_rgb { channels } {
-    foreach { red green blue } $channels {
-        set r [expr { $red*0.0002442002442002442 }]
-        set g [expr { $green*0.0002442002442002442 }]
-        set b [expr { $blue*0.0002442002442002442 }]
-
-        # Adjust the rgb channels values if they exceeds their limits [0,1.0].
-        if { $r < 0 } {
-            set r 0
-        } elseif { $r > 1.0 } {
-            set r 1.0
-        }
-
-        if { $g < 0 } {
-            set g 0
-        } elseif { $g > 1.0 } {
-            set g 1.0
-        }
-
-        if { $b < 0 } {
-            set b 0
-        } elseif { $b > 1.0 } {
-            set b 1.0
-        }
+    foreach { red12 green12 blue12 } $channels {
+        set r [expr { $red12*0.0002442002442002442   }]
+        set g [expr { $green12*0.0002442002442002442 }]
+        set b [expr { $blue12*0.0002442002442002442  }]
 
         lappend results $r $g $b
     }
