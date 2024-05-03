160dd89fbe443065ed10fa89118786460f6950df
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:09:34 +0200

    refactor: HSP to HSB conversion (without alpha channel)

--- colormath/_HSP_HSB.tcl
+++ colormath/_HSP_HSB.tcl
@@ -4,45 +4,69 @@
 
 # ::_HSP_HSB
 #
-# Transform HSP channels into HSB channels.
+# Transform HSP colors (without alpha channel) into HSB colors (without alpha channel).
 #
 # Where:
 #
-# channels      Should be a list containing the HSP channels to convert, with:
-#                   the hue channels in the range [0,360[,
-#                   the saturation channels in the range [0,100.0],
-#                   the perceived brightness channels in the range [0,100.0].
+# channels      Should be a list that specifies all the channels (flattened together) of the HSP colors to convert.
+#               Each HSP color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   H --> Hue                  [0,360.0[ --> Note: '360.0' is not included.
+#                   S --> Saturation           [0,100.0]
+#                   P --> Perceived Brightness [0,100.0]
 #
-# Note:  Some pre-computation have been made in order to increase the performance:
-#           1.0/6.0   = 0.16666666666666666
-#           2.0/6.0   = 0.3333333333333333
-#           3.0/6.0   = 0.5
-#           4.0/6.0   = 0.6666666666666666
-#           5.0/6.0   = 0.8333333333333334
-#           1.0/100.0 = 0.01
-#           1.0/360.0 = 0.002777777777777778
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
+#
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 120 50 50]
+#                       channels --> [list 120 50 50]
+#
+#                   Two colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       channels --> [list 120 50 50 57 80 80] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 120 50 50]
+#                       color2   --> [list 57  80 80]
+#                       color3   --> [list 270 20 90]
+#                       channels --> [list 120 50 50 57 80 80 270 20 90] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# Some pre-computation have been made in order to increase the performance:
+#   1 / 6   = 0.16666666666666666
+#   2 / 6   = 0.3333333333333333
+#   3 / 6   = 0.5
+#   4 / 6   = 0.6666666666666666
+#   5 / 6   = 0.8333333333333334
+#   1 / 100 = 0.01
+#   1 / 360 = 0.002777777777777778
 #
 # Note:  For info about unadapted values visit 'http://www.brucelindbloom.com'.
 #
 # Note:  For info about the HSP color system visit 'https://www.alienryderflex.com/hsp.html'.
 #
-# Returns a list containing the resulting HSB channels, with:
-#   the hue channels in the range [0,360[,
-#   the saturation channels in the range [0,100.0],
-#   the brightness channels in the range [0,100.0].
+# Return a list containing the resulting HSB colors channels (flattened together).
+# Each HSB color will be rappresented by 3 channels values in the following order and ranges:
+#   H --> Hue        [0,360.0[ --> Note: '360.0' is not included.
+#   S --> Saturation [0,100.0]
+#   B --> Brightness [0,100.0]
 proc ::_HSP_HSB { channels } {
     set Yr $::sRGB(unadapted,Yr)
     set Yg $::sRGB(unadapted,Yg)
     set Yb $::sRGB(unadapted,Yb)
 
     foreach { hue saturation perceived_brightness } $channels {
-        set h  [expr { $hue*0.002777777777777778 }]; # [0,1.0]
-        set s  [expr { $saturation*0.01 }]; # [0,1.0]
-        set pb [expr { $perceived_brightness*0.01 }]; # [0,1.0]
+        set h  [expr { $hue*0.002777777777777778  }]; # range [0,1.0]
+        set s  [expr { $saturation*0.01           }]; # range [0,1.0]
+        set pb [expr { $perceived_brightness*0.01 }]; # range [0,1.0]
 
         set minovermax [expr (1.0-$s)]
 
-        # Compute the rgb values.
+        # Compute the rgb values [0,1.0].
         if { $minovermax > 0 } {
             set fraction [expr { 1.0/$minovermax }]
 
@@ -154,25 +178,6 @@ proc ::_HSP_HSB { channels } {
             }
         }
 
-        # Adjust the rgb channels values if they exceeds their limits [0,1.0].
-        if { $r < 0 } {
-            set r 0
-        } elseif { $r > 1.0 } {
-            set r 1.0
-        }
-
-        if { $g < 0 } {
-            set g 0
-        } elseif { $g > 1.0 } {
-            set g 1.0
-        }
-
-        if { $b < 0 } {
-            set b 0
-        } elseif { $b > 1.0 } {
-            set b 1.0
-        }
-
         set min  [expr { min($r,$g,$b) }]
         set max  [expr { max($r,$g,$b) }]
         set diff [expr { $max-$min }]
@@ -180,30 +185,14 @@ proc ::_HSP_HSB { channels } {
         # Compute the brightness [0,100.0].
         set brightness [expr { $max*100.0 }]
 
-        # Adjust the brightness value if exceeds its limits [0,100.0].
-        if { $brightness < 0 } {
-            set brightness 0
-        } elseif { $brightness > 100.0 } {
-            set brightness 100.0
-        }
-
+        # Compute the saturation [0,100.0].
         switch -- $diff {
             0   {
                 # It's a gray...
                 set hue        0
                 set saturation 0
             }
-            default {
-                # Compute the saturation [0,100.0].
-                set saturation [expr { ($diff/$max)*100.0 }]
-
-                # Adjust the saturation value if exceeds its limits [0,100.0].
-                if { $saturation < 0 } {
-                    set saturation 0
-                } elseif { $saturation > 100.0 } {
-                    set saturation 100.0
-                }
-            }
+            default { set saturation [expr { ($diff/$max)*100.0 }] }
         }
 
         lappend results $hue $saturation $brightness
