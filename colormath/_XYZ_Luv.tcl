565173cd102e007b01fb8102b85a7563714982db
Roberto Calabrese <2610806+robertocalabrese@users.noreply.github.com>
2024-05-03 11:12:55 +0200

    refactor: XYZ to Luv conversion (without alpha channel)

--- colormath/_XYZ_Luv.tcl
+++ colormath/_XYZ_Luv.tcl
@@ -4,24 +4,51 @@
 
 # ::_XYZ_Luv
 #
-# Transform XYZ channels into Luv channels (both in the PCS D50 space).
+# Transform XYZ colors (without alpha channel) into Luv colors (without alpha channel).
+# Both in the PCS D50 space.
 #
 # Where:
 #
-# channels      Should be a list containing the XYZ channels values to convert, with:
-#                   the X channels in the range [0,$sRGB(PCS,X)],
-#                   the Y channels in the range [0,$sRGB(PCS,Y)],
-#                   the Z channels in the range [0,$sRGB(PCS,Z)].
+# channels      Should be a list that specifies all the channels (flattened together) of the XYZ colors to convert.
+#               Each XYZ color needs to be rappresented by 3 channels values in the following order and ranges:
+#                   X --> X [0,$sRGB(PCS,X)]
+#                   Y --> Y [0,$sRGB(PCS,Y)]
+#                   Z --> Z [0,$sRGB(PCS,Z)]
 #
-# Note:  Some pre-computation have been made in order to increase the performance:
-#           1.0/3.0       = 0.3333333333333333
-#           24389.0/27.0  = 903.2962962962963
-#           216.0/24389.0 = 0.008856451679035631
+#               Attention, the input and output colors will not be checked.
+#               Please, take the appropriate steps before and after using this procedure or use the color command instead.
 #
-# Returns a list containing the resulting Luv channels, with:
-#   the L channels in the range [0,100.0],
-#   the u channels in the range [-134.0,220.0],
-#   the v channels in the range [-140.0,122.0].
+#               Examples:
+#
+#                   One color:
+#                       color    --> [list 0.4 0.5 0.2]
+#                       channels --> [list 0.4 0.5 0.2]
+#
+#                   Two colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6] <-- all colors channels must be flattened together.
+#
+#                   Three colors:
+#                       color1   --> [list 0.4 0.5 0.2]
+#                       color2   --> [list 0.5 0.4 0.6]
+#                       color3   --> [list 0.2 0.2 0.1]
+#                       channels --> [list 0.4 0.5 0.2 0.5 0.4 0.6 0.2 0.2 0.1] <-- all colors channels must be flattened together.
+#
+#                   and so on and so forth...
+#
+# Some pre-computation have been made in order to increase the performance:
+#   1 / 3       = 0.3333333333333333
+#   216 / 24389 = 0.008856451679035631
+#   24389 / 27  = 903.2962962962963
+#
+# Note:  For info about XYZ to Luv conversions visit 'http://www.brucelindbloom.com'.
+#
+# Return a list containing the resulting Luv colors channels (flattened together).
+# Each Luv color will be rappresented by 3 channels values in the following order and ranges:
+#   L --> Lightness [0,100.0]
+#   u --> u         [-134.0,220.0]
+#   v --> v         [-140.0,122.0]
 proc ::_XYZ_Luv { channels } {
     switch -- $::CIE {
         standard {
@@ -49,38 +76,19 @@ proc ::_XYZ_Luv { channels } {
             set u1 [expr { (4.0*$X)/($X+(15.0*$Y)+(3.0*$Z)) }]
             set v1 [expr { (9.0*$Y)/($X+(15.0*$Y)+(3.0*$Z)) }]
 
+            # Compute the lightness [0,100.0].
             # yr = $Y/$Y_ref = $Y
             if { $Y > $epsilon } {
                 set L [expr { (116.0*(pow($Y,0.3333333333333333)))-16.0 }]
             } else {
                 set L [expr { $k*$Y }]
             }
+
+            # Compute u [-134.0,220.0].
             set u [expr { 13.0*$L*($u1-$u1r) }]
+
+            # Compute v [-140.0,122.0].
             set v [expr { 13.0*$L*($v1-$v1r) }]
-
-            ## Rounding to the 12th decimal position to avoid exponential result.
-            #set L [round $L 12]
-            #set u [round $u 12]
-            #set v [round $v 12]
-
-            # Adjust the Luv values if they exceeds their limits.
-            if { $L < 0 } {
-                set L 0
-            } elseif { $L > 100.0 } {
-                set L 100.0
-            }
-
-            if { $u < -134.0 } {
-                set u -134.0
-            } elseif { $u > 220.0 } {
-                set u 220.0
-            }
-
-            if { $v < -140.0 } {
-                set v -140.0
-            } elseif { $v > 122.0 } {
-                set v 122.0
-            }
         }
 
         lappend results $L $u $v
